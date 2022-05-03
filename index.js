274423a09c25f9b65b105ba8d5355516d33a6dae
ashutoshvish07 <101579327+ashutoshvish07@users.noreply.github.com>
2022-05-03 17:44:52 +0530

    progressing

---
+++ index.js
@@ -0,0 +1,6 @@
+// https://logan.nnnow.com/content/dam/nnnow-project/11-april-2022/SC_Homepage_Beautypowerforall_Desktop.jpg
+// https://logan.nnnow.com/content/dam/nnnow-project/09-march-2022/Benefit_TopbannerDesktop(2).jpg
+//https://logan.nnnow.com/content/dam/nnnow-project/31-mar-2022/se/SC_Topbanner_Newatsephoradesktop.jpg
+// https://logan.nnnow.com/content/dam/nnnow-project/22-april-2022/ABH_TopBanner_ABHMattelipstickslaunchDESKTOP.gif
+// https://logan.nnnow.com/content/dam/nnnow-project/22-april-2022/Clinique_TopBannerDESKTOP.jpg
+// https://logan.nnnow.com/content/dam/nnnow-project/13-april-2022/se/Sephora_TopBanner_Summer,Sunshine&SPFDesktop.jpg
